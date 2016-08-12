ccaff0c05df02972053056793a40a945fa504ae7
Yann LERJEN <lindel13@hotmail.com>
2016-08-12 16:03:00 +0200

    remove server.js node file

--- server.js
+++
@@ -1,5 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(6789, function(){
-    console.log('Server running on 6789...');
-});
